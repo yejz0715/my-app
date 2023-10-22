4f7a5bfe06ee4c58286045a9d52dfb969313a3d5
yejz0715 <yejz0715@gmail.com>
2023-10-22 14:17:04 +0900

    sidebar를 mainPage로 이동

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import "./style.scss";
+import Sidebar from "../../components/Sidebar";
 const Main = () => {
-    return <div className="main-container">main</div>;
+    return (
+        <div className="main-container">
+            <Sidebar />
+            <h1>main</h1>
+        </div>
+    );
 };
 
 export default Main;
