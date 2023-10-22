e1ae4d9e4a2bae5448ca1aaf439ca7c3312aab1a
yejz0715 <yejz0715@gmail.com>
2023-10-22 14:48:12 +0900

    update mainPage

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./style.scss";
 import Sidebar from "../../components/Sidebar";
+import { useLocation } from "react-router-dom";
+import Details from "../Details";
 const Main = () => {
+    const location = useLocation();
+    console.log(location.pathname);
     return (
         <div className="main-container">
             <Sidebar />
-            <h1>main</h1>
+            <main className="main-detail">
+                {location.pathname === "/" ? <h1>main</h1> : <Details />}
+            </main>
         </div>
     );
 };
