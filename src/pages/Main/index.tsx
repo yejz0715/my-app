8fb70cbb4aec2bb43080f35fdc43872e26df44ea
yejz0715 <yejz0715@gmail.com>
2023-10-23 22:10:47 +0900

    mainPage api_getContents 추가

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.scss";
 import Sidebar from "../../components/Sidebar";
 import { useLocation } from "react-router-dom";
 import Details from "../Details";
+import { mainApi } from "../../api";
+import { chapterType } from "@src/types/sidebar";
 const Main = () => {
     const location = useLocation();
-    console.log(location.pathname);
+    const [contents, setContents] = useState<chapterType[]>([]);
+    const getAllContent = async () => {
+        try {
+            const { data } = await mainApi.getContents();
+            setContents(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    useEffect(() => {
+        getAllContent();
+    }, []);
     return (
         <div className="main-container">
-            <Sidebar />
+            <Sidebar contents={contents} />
             <main className="main-detail">
                 {location.pathname === "/" ? <h1>main</h1> : <Details />}
             </main>
