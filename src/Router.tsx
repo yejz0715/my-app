a92bd08e5becaed570405e025b91a66e230e9fc6
yejz0715 <yejz0715@gmail.com>
2023-10-29 11:28:09 +0900

    router_route path 수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import Details from "./pages/Details";
+import DefaultSelect from "./components/Select/DefaultSelect";
+import CheckBoxSelect from "./components/Select/CheckBoxSelect";
+import InputSelect from "./components/Select/InputSelect";
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Main />}>
-                    <Route path="/details/:subId" element={<Details />} />
+                    <Route path="/details/a01" element={<DefaultSelect />} />
+                    <Route path="/details/a02" element={<CheckBoxSelect />} />
+                    <Route path="/details/a03" element={<InputSelect />} />
                 </Route>
             </Routes>
         </BrowserRouter>
