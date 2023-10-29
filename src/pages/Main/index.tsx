f8f9c830710d15e5530601ae6c380fc1bdeabbbd
yejz0715 <yejz0715@gmail.com>
2023-10-29 11:30:50 +0900

    path에 따라 컴포넌트 이동 로직 수정

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
 import "./style.scss";
 import Sidebar from "../../components/Sidebar";
-import { useLocation } from "react-router-dom";
-import Details from "../Details";
+import { useLocation, useParams } from "react-router-dom";
 import { mainApi } from "../../api";
 import { chapterType } from "@src/types/sidebar";
+import Accordion from "../../components/Accordion";
+import DefaultSelect from "../../components/Select/DefaultSelect";
+import InputSelect from "../../components/Select/InputSelect";
+import CheckBoxSelect from "../../components/Select/CheckBoxSelect";
 const Main = () => {
     const location = useLocation();
+    const param = useParams();
     const [contents, setContents] = useState<chapterType[]>([]);
     const getAllContent = async () => {
         try {
@@ -19,14 +23,33 @@ const Main = () => {
     useEffect(() => {
         getAllContent();
     }, []);
+
+    const changeDetail = () => {
+        if (location.pathname === "/") {
+            return (
+                <>
+                    <h1>main</h1>
+                    <Accordion />
+                </>
+            );
+        }
+
+        if (location.pathname === "/details/a01") {
+            return <DefaultSelect />;
+        }
+        if (location.pathname === "/details/a02") {
+            return <CheckBoxSelect />;
+        }
+        if (location.pathname === "/details/a03") {
+            return <InputSelect />;
+        }
+    };
+
     return (
         <div className="main-container">
             <Sidebar contents={contents} />
-            <main className="main-detail">
-                {location.pathname === "/" ? <h1>main</h1> : <Details />}
-            </main>
+            <main className="main-detail">{changeDetail()}</main>
         </div>
     );
 };
-
 export default Main;
