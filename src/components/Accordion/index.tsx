acbb20467c8467e9e2fe2a9cb1adf28f46d3ef33
yejz0715 <yejz0715@gmail.com>
2023-11-02 23:58:36 +0900

    feat: components_Accordion에 RGB 색상 복사 기능 추가

--- src/components/Accordion/index.tsx
+++ src/components/Accordion/index.tsx
@@ -1,38 +1,40 @@
 import React, { useState } from "react";
-import imgRed from "../../assets/red.png";
-import imgBlue from "../../assets/blue.png";
-import imgBlack from "../../assets/black.png";
-import { cardType } from "@src/types/sidebar";
 import "./style.scss";
+import { cards } from "../../data/dummy";
+
 const Accordion = () => {
-    const cards: cardType[] = [
-        {
-            name: "red",
-            imgUrl: imgRed,
-        },
-        {
-            name: "blue",
-            imgUrl: imgBlue,
-        },
-        {
-            name: "black",
-            imgUrl: imgBlack,
-        },
-    ];
     const [active, setActive] = useState(0);
+    const copyOnClick = async (text: string) => {
+        try {
+            await navigator.clipboard.writeText(`${text}`);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     return (
-        <section className="accordion-container">
-            {cards.map((item, index) => (
-                <article
-                    key={index}
-                    onClick={() => setActive(index)}
-                    className={active === index ? "active" : ""}
-                >
-                    <img src={item.imgUrl} alt={item.name} />
-                </article>
-            ))}
-        </section>
+        <>
+            <section className="accordion-container">
+                {cards.map((item, index) => (
+                    <article
+                        key={index}
+                        onClick={() => setActive(index)}
+                        className={active === index ? "active" : ""}
+                    >
+                        <img src={item.imgUrl} alt={item.name} />
+                        <div className="content">
+                            <span id="name">{item.name}</span>
+                            <div className="rgb-content">
+                                <span>{item.rgb}</span>
+                                <button onClick={() => copyOnClick(item.rgb)}>
+                                    copy
+                                </button>
+                            </div>
+                        </div>
+                    </article>
+                ))}
+            </section>
+        </>
     );
 };
 export default Accordion;
