f7df87332db6cbc1e11ee345a4a1e455eb6fa59c
yejz0715 <yejz0715@gmail.com>
2023-11-03 01:51:17 +0900

    fix: components_Accordion 데이터 수정

---
+++ src/data/dummy.ts
@@ -0,0 +1,46 @@
+import { cardType } from "@src/types/components";
+import imgRed from "../assets/accordion/red.png";
+import imgOrange from "../assets/accordion/orange.png";
+import imgYellow from "../assets/accordion/yellow.png";
+import imgGreen from "../assets/accordion/green.png";
+import imgBlue from "../assets/accordion/blue.png";
+import imgNavy from "../assets/accordion/navy.png";
+import imgPurple from "../assets/accordion/purple.png";
+
+export const cards: cardType[] = [
+    {
+        name: "red",
+        imgUrl: imgRed,
+        rgb: "#FF0000",
+    },
+    {
+        name: "orange",
+        imgUrl: imgOrange,
+        rgb: "#FFA500",
+    },
+    {
+        name: "yellow",
+        imgUrl: imgYellow,
+        rgb: "#FFFF00",
+    },
+    {
+        name: "green",
+        imgUrl: imgGreen,
+        rgb: "#008000",
+    },
+    {
+        name: "blue",
+        imgUrl: imgBlue,
+        rgb: "#0000FF",
+    },
+    {
+        name: "navy",
+        imgUrl: imgNavy,
+        rgb: "#000080",
+    },
+    {
+        name: "purple",
+        imgUrl: imgPurple,
+        rgb: "#800080",
+    },
+];
