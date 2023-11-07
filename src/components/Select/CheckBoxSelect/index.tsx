95966213236b369ea43422800380a95e708a9ef1
yejz0715 <yejz0715@gmail.com>
2023-11-07 14:49:27 +0900

    delete components

--- src/components/Select/CheckBoxSelect/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-const CheckBoxSelect = () => {
-    return (
-        <>
-            <h1>checkBox select</h1>
-        </>
-    );
-};
-export default CheckBoxSelect;
--- src/components/Select/DefaultSelect/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-const DefaultSelect = () => {
-    return (
-        <section>
-            <h1>default select</h1>
-        </section>
-    );
-};
-export default DefaultSelect;
