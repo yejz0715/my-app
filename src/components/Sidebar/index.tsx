46baab3c28d99e889fe311c68dff69c3ce307df5
yejz0715 <yejz0715@gmail.com>
2023-12-01 20:12:40 +0900

    component_Sidebar 주석+타입설정+로직수정

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,24 +1,29 @@
 import React from "react";
 import "./style.scss";
 import { Link } from "react-router-dom";
-import { chapterType } from "@src/types/components";
+import { chapterType, sidebarProps } from "@src/types/components";
 
-interface SidebarProps {
-    contents: chapterType[];
-}
-const Sidebar = ({ contents }: SidebarProps) => {
+/**
+ * Sidebar component 입니다.
+ * @param { sidebarProps } contents
+ * @returns {JSX.Element}
+ */
+const Sidebar = ({ contents }: sidebarProps): JSX.Element => {
+    //클릭 이벤트 함수_ 해당 HTML 요소를 찾아서 서브 네비게이션을 열거나 닫아줍니다.
     const handleOnClick = (name: string) => {
         const subNav = document.querySelector<HTMLElement>(`#${name}`);
         const subNavInner = document.querySelector<HTMLElement>(
             `#${name} .sub-nav-inner`
         );
 
-        if (subNav?.clientHeight === 0) {
-            subNav?.classList.add("open");
-            subNav!!.style.height = `${subNavInner?.clientHeight}px`;
+        if (!subNav || !subNavInner) return;
+
+        if (subNav.clientHeight === 0) {
+            subNav.classList.add("open");
+            subNav.style.height = `${subNavInner.clientHeight}px`;
         } else {
-            subNav?.classList.remove("open");
-            subNav!!.style.height = `0px`;
+            subNav.classList.remove("open");
+            subNav.style.height = `0px`;
         }
     };
 
@@ -26,47 +31,50 @@ const Sidebar = ({ contents }: SidebarProps) => {
         <aside className="sidebar-container">
             <nav className="sidebar-nav">
                 <header className="sidebar-header">StudyBook</header>
-                {contents.map((chapter) => (
-                    <div key={chapter.id}>
-                        {chapter.children.length === 0 ? (
-                            <button className="sidebar-item">
-                                <Link
-                                    to={`/details/${chapter.id}`}
-                                    className="sidebar-item-label"
-                                >
-                                    <span>{chapter.name}</span>
-                                </Link>
-                            </button>
-                        ) : (
-                            <>
-                                <button
-                                    className="sidebar-item"
-                                    onClick={() => handleOnClick(chapter.name)}
-                                >
-                                    <span>{chapter.name}</span>
+                {contents &&
+                    contents.map((chapter) => (
+                        <div key={chapter.id}>
+                            {chapter.children.length === 0 ? (
+                                <button className="sidebar-item">
+                                    <Link
+                                        to={`/details/${chapter.id}`}
+                                        className="sidebar-item-label"
+                                    >
+                                        <span>{chapter.name}</span>
+                                    </Link>
                                 </button>
+                            ) : (
+                                <>
+                                    <button
+                                        className="sidebar-item"
+                                        onClick={() =>
+                                            handleOnClick(chapter.name)
+                                        }
+                                    >
+                                        <span>{chapter.name}</span>
+                                    </button>
 
-                                <div className="sub-nav" id={chapter.name}>
-                                    <div className="sub-nav-inner">
-                                        {chapter.children.map((item) => (
-                                            <button
-                                                key={item.id}
-                                                className="sidebar-item"
-                                            >
-                                                <Link
-                                                    to={`/details/${item.id}`}
-                                                    className="sidebar-item-children-label"
+                                    <div className="sub-nav" id={chapter.name}>
+                                        <div className="sub-nav-inner">
+                                            {chapter.children.map((item) => (
+                                                <button
+                                                    key={item.id}
+                                                    className="sidebar-item"
                                                 >
-                                                    <span>{item.name}</span>
-                                                </Link>
-                                            </button>
-                                        ))}
+                                                    <Link
+                                                        to={`/details/${item.id}`}
+                                                        className="sidebar-item-children-label"
+                                                    >
+                                                        <span>{item.name}</span>
+                                                    </Link>
+                                                </button>
+                                            ))}
+                                        </div>
                                     </div>
-                                </div>
-                            </>
-                        )}
-                    </div>
-                ))}
+                                </>
+                            )}
+                        </div>
+                    ))}
             </nav>
         </aside>
     );
