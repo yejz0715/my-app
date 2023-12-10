eafedca31466825396feff0b68bb0dff23831b7f
yejz0715 <yejz0715@gmail.com>
2023-12-10 23:23:32 +0900

    component_sidebar 수정중

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.scss";
 import { Link } from "react-router-dom";
-import { chapterType, sidebarProps } from "@src/types/components";
+import { sidebarProps } from "@src/types/components";
 
 /**
  * Sidebar component 입니다.
@@ -9,6 +9,8 @@ import { chapterType, sidebarProps } from "@src/types/components";
  * @returns {JSX.Element}
  */
 const Sidebar = ({ contents }: sidebarProps): JSX.Element => {
+    const [isActive, setIsActive] = useState("");
+
     //클릭 이벤트 함수_ 해당 HTML 요소를 찾아서 서브 네비게이션을 열거나 닫아줍니다.
     const handleOnClick = (name: string) => {
         const subNav = document.querySelector<HTMLElement>(`#${name}`);
@@ -31,6 +33,7 @@ const Sidebar = ({ contents }: sidebarProps): JSX.Element => {
         <aside className="sidebar-container">
             <nav className="sidebar-nav">
                 <header className="sidebar-header">StudyBook</header>
+
                 {contents &&
                     contents.map((chapter) => (
                         <div key={chapter.id}>
